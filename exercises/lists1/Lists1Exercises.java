1f8a3ac1c634c360398d1960b121f1c6f8e69093
PinChi <sueric68@gmail.com>
2020-04-26 00:48:40 +0800

    終於完成~過一陣子再想或再看一些後面課程容易得到答案~~

--- exercises/lists1/Lists1Exercises.java
+++ exercises/lists1/Lists1Exercises.java
@@ -4,12 +4,21 @@ public class Lists1Exercises {
       * to change. */
     public static IntList incrList(IntList L, int x) {
         /* Your code here. */
+        //use L1 to remember the origin L's address,and use iterative:))
         IntList L1 = L;
-        int i = 0;
-        while(i < L1.size()){
-            L = new IntList(L1.get(i) + x, null);
-            i++
+        int j = L1.size() - 1;
+        for(int i = 0; i < L1.size(); i++){
+            if(i == 0){
+                L = new IntList(L1.get(j) + x, null);
+                j--;
+                continue;
+            }
+            else{
+                L = new IntList(L1.get(j) + x, L);
+                j--;
+            }
         }
+        
         return L;        
     }
 
@@ -18,10 +27,13 @@ public class Lists1Exercises {
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
         /* Your code here. */
-        while(L.rest != null){
+        //creat a IntList L1 to remember the origin L's address,after iterative,let L equals L1~
+        IntList L1 = L;
+        while(L != null){
             L.first += x;
             L = L.rest;
         }
+        L = L1;
         return L;
     }
 
@@ -29,13 +41,13 @@ public class Lists1Exercises {
         IntList L = new IntList(5, null);
         L.rest = new IntList(7, null);
         L.rest.rest = new IntList(9, null);
-        System.out.println(incrList(L, 3));
+        System.out.println(dincrList(L, 3));
         //System.out.println(L.iterativeSize());
 
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
-        // System.out.println(L.get(1));
+         System.out.println(L.get(1));
         // System.out.println(incrList(L, 3));
         // System.out.println(dincrList(L, 3));        
     }
